35d7a5d5151e4ca9967b4718ab7c9cbac9d8212e
“Klariskao” <ktsai@csob.cz>
2025-03-21 11:10:27 +0100

    ♻️ Update MainActivity with navigation

--- app/src/main/java/com/example/flashcards/MainActivity.kt
+++ app/src/main/java/com/example/flashcards/MainActivity.kt
@@ -1,7 +1,19 @@
 package com.example.flashcards
 
-import android.app.Application
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
+import com.example.flashcards.ui.AppNavigation
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainActivity : Application()
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            AppNavigation(navController)
+        }
+    }
+}
