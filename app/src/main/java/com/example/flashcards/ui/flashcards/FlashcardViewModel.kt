d26298082559b431616be290b58b7f33a1b207ea
“Klariskao” <ktsai@csob.cz>
2025-03-21 11:08:01 +0100

    ✨ Add FlashcardViewModel

---
+++ app/src/main/java/com/example/flashcards/ui/flashcards/FlashcardViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.flashcards.ui.flashcards
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.flashcards.data.model.Vocabulary
+import com.example.flashcards.repository.VocabularyRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FlashcardViewModel @Inject constructor(private val repository: VocabularyRepository) : ViewModel() {
+    private val _words = MutableStateFlow<List<Vocabulary>>(emptyList())
+    val words: StateFlow<List<Vocabulary>> = _words
+
+    init {
+        loadWords()
+    }
+
+    private fun loadWords() {
+        viewModelScope.launch {
+            repository.allWords.collect { wordList ->
+                _words.value = wordList
+            }
+        }
+    }
+
+    fun markRight(word: Vocabulary) {
+        viewModelScope.launch {
+            repository.updateWord(word.copy(score = word.score + 1))
+        }
+    }
+
+    fun markWrong(word: Vocabulary) {
+        viewModelScope.launch {
+            repository.updateWord(word.copy(score = word.score - 1))
+        }
+    }
+}
